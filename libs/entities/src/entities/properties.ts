da8965546b066000c1f90452afda639aa25706a9
JustEatAnApple <mblacioti@gmail.com>
2024-08-08 10:51:04 +0300

    Teamwork Template

--- libs/entities/src/entities/properties.ts
+++ libs/entities/src/entities/properties.ts
@@ -2,10 +2,32 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
+export class Payment {
+    @ApiProperty()
+    @IsString()
+    token_identifier!: string;
+
+    @ApiProperty()
+    @IsNumber()
+    token_nonce!: number;
+
+    @ApiProperty()
+    @IsNumber()
+    amount!: number;
+}
+
+export class PaymentList {
+    @ApiProperty({ type: [Payment] })
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => Payment)
+    tokens!: Payment[];
+}
+
 export class TokenIdentifier {
     @ApiProperty()
     @IsString()
-    tokenID!: string;
+    token_identifier!: string;
 }
 
 export class TokenIdentifierList {
